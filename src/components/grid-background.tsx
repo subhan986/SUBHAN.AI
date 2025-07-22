1c4926dfbd09c11c61de7418295d742c043b9f5d
Muhammad Subhan <subyounas@gmail.com>
2025-07-22 08:25:21 +0000

    remove the bg mouse reacted on entire website exceot the hero

--- src/components/grid-background.tsx
+++ src/components/grid-background.tsx
@@ -3,25 +3,10 @@
 import React from "react";
 
 export function GridBackground({ children }: { children: React.ReactNode }) {
-  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
-
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    const { clientX, clientY, currentTarget } = event;
-    const { left, top } = currentTarget.getBoundingClientRect();
-    setMousePosition({ x: clientX - left, y: clientY - top });
-  };
-
   return (
     <div
-      className="relative w-full bg-background group"
-      onMouseMove={handleMouseMove}
+      className="relative w-full bg-background"
     >
-      <div
-        className="absolute inset-0 z-0 transition-all duration-300 opacity-0 group-hover:opacity-100"
-        style={{
-          background: `radial-gradient(circle at ${mousePosition.x}px ${mousePosition.y}px, hsl(var(--primary)/0.15), transparent 40%)`,
-        }}
-      />
        <div
         className="absolute inset-0 z-0"
         style={{
