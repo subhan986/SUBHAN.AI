021595509367fc6b406ab77710af480b7e90bf63
Muhammad Subhan <subyounas@gmail.com>
2026-03-12 10:49:53 +0000

    make sure when somone puts a review it is visible to everyone also what

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBV7_JTjfU-NOe0UYX3atM6edtK28nH5JE",
-  authDomain: "visionaryai-fm9rv.firebaseapp.com",
-  projectId: "visionaryai-fm9rv",
-  storageBucket: "visionaryai-fm9rv.firebasestorage.app",
-  messagingSenderId: "52296178628",
-  appId: "1:52296178628:web:e446891c91a80fdf81e291"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
